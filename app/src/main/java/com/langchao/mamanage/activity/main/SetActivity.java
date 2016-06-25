ef20ac3eca0aca0b451ef4bf9d8ad54438b95a1b
NickMiaohl <miaohl@aliyun.com>
2016-06-25 23:27:26 +0800

    add net  db,miaohl

--- app/src/main/java/com/langchao/mamanage/activity/main/SetActivity.java
+++ app/src/main/java/com/langchao/mamanage/activity/main/SetActivity.java
@@ -1,12 +1,14 @@
 package com.langchao.mamanage.activity.main;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.langchao.mamanage.R;
+import com.langchao.mamanage.activity.LoginActivity;
 import com.langchao.mamanage.activity.ServiceActivity;
 import com.langchao.mamanage.common.MaConstants;
 import com.zhy.autolayout.AutoLayoutActivity;
@@ -31,6 +33,8 @@ public class SetActivity extends AutoLayoutActivity {
     @ViewInject(R.id.textView24)
     private TextView ip;
 
+
+
     Intent intent = new Intent();
 
     @Override
@@ -43,7 +47,7 @@ public class SetActivity extends AutoLayoutActivity {
     }
 
     //点击事件
-    @Event(value = {R.id.service, R.id.set}, type = View.OnClickListener.class)
+    @Event(value = {R.id.service, R.id.set,R.id.logOut}, type = View.OnClickListener.class)
     private void onButtonClick(View v) {
         switch (v.getId()) {
             case R.id.service:
@@ -54,7 +58,30 @@ public class SetActivity extends AutoLayoutActivity {
                 intent.setClass(this, ServiceActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.logOut://参数设置
+                clearUserInfo();
+                Intent intent = new Intent();
+                intent.setAction(MaConstants.CLOSE); // 说明动作
+                sendBroadcast(intent);// 该函数用于发送广播
+                finish();
+                SetActivity.this.finish();
+                intent.setClass(this, LoginActivity.class);
+                startActivity(intent);
+                break;
         }
+    }
+
+    private void clearUserInfo() {
+
+            SharedPreferences.Editor editor = getSharedPreferences(MaConstants.FILENAME, MODE_PRIVATE).edit();
+            // 存入键值对
+            editor.remove(MaConstants.PARA_USERNAME);
+            editor.remove(MaConstants.PARA_USERID);
+            editor.remove(MaConstants.PARA_AUTOLOGIN);
+
+            // 将内存中的数据写到XML文件中去
+            editor.commit();
+
     }
 
 
