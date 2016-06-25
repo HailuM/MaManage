aa0ca38a5fbb76bcedf1d1be9fe073ad92166c18
柏毛 <840924057@qq.com>
2016-06-25 19:07:24 +0800

    Initial commit

--- app/src/main/java/com/langchao/mamanage/activity/main/InStorageActivity.java
+++ app/src/main/java/com/langchao/mamanage/activity/main/InStorageActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 
 import com.langchao.mamanage.R;
 
+import org.xutils.view.annotation.ContentView;
+import org.xutils.x;
+
+@ContentView(R.layout.activity_in_storage)
 public class InStorageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_in_storage);
+        x.view().inject(this);
     }
 }
