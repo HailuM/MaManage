62932fa7f089a02e6dc994e70ae017d7710120c0
NickMiaohl <miaohl@aliyun.com>
2016-07-02 21:34:42 +0800

    bluetooth print,miaohl

--- app/src/main/java/com/langchao/mamanage/lcprint/BluetoothActivity.java
+++ app/src/main/java/com/langchao/mamanage/lcprint/BluetoothActivity.java
@@ -4,16 +4,27 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
 import com.langchao.mamanage.R;
 
+import org.xutils.view.annotation.Event;
+import org.xutils.x;
+
 public class BluetoothActivity extends Activity {
     private Context context = null;
-  
+
+    @Event(value = {R.id.back_image}, type = View.OnClickListener.class)
+    private void back(View v) {
+        this.finish();
+
+    }
+
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);  
+        super.onCreate(savedInstanceState);
+        x.view().inject(this);
         this.context = this;  
         setTitle("蓝牙打印");  
         setContentView(R.layout.bluetooth_layout);
@@ -43,10 +54,10 @@ public class BluetoothActivity extends Activity {
     //屏蔽返回键的代码:  
     public boolean onKeyDown(int keyCode,KeyEvent event)
     {  
-        switch(keyCode)  
-        {  
-            case KeyEvent.KEYCODE_BACK:return true;  
-        }  
+//        switch(keyCode)
+//        {
+//           // case KeyEvent.KEYCODE_BACK:return true;
+//        }
         return super.onKeyDown(keyCode, event);  
     }  
 }
